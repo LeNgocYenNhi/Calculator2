816845847d0e5f3bb3a18740966657c51382bbf5
LeNgocYenNhi <82575213+LeNgocYenNhi@users.noreply.github.com>
2021-06-15 23:38:27 +0700

    Create MenuItemListener.java

---
+++ src/Calculation/MenuItemListener.java
@@ -0,0 +1,22 @@
+package Calculation;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuItemListener implements ActionListener {
+    public void actionPerformed(ActionEvent e) {
+    	  if(e.getActionCommand() == "Standard") {
+    		    new StandardCalculator();
+    	  }
+    	  if(e.getActionCommand() == "Scientific") {
+    		    new ScienceCalculator();
+    	  }
+        if(e.getActionCommand()== "Length") {
+          	new Length();
+        }
+        if(e.getActionCommand() == "Weight") {
+          	new Weight();
+        }
+
+    }
+}
