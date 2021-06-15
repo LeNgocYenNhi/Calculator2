4ab0675c02cdff0f3a21524ddbf3ac508df4d6d8
LeNgocYenNhi <82575213+LeNgocYenNhi@users.noreply.github.com>
2021-06-15 23:23:49 +0700

    Update ScienceCalculator.java

--- src/Calculation/ScienceCalculator.java
+++ src/Calculation/ScienceCalculator.java
@@ -11,6 +11,7 @@ public class ScienceCalculator extends Calculator implements ActionListener{
 	protected HistoryField HistoryArea;
 	protected SymbolTable SymTable;
 	protected Processor Processor;
+	protected Menu menu;
 
 	public ScienceCalculator() {
 		CalPanel = getCalculatingPanel();
@@ -18,22 +19,27 @@ public class ScienceCalculator extends Calculator implements ActionListener{
 		HistoryArea = getHistoryField();
 		SymTable = getSymbolTable();
 		Processor = getProcessor();
+		Menu = getMenu();
+		
 
 		frame = new JFrame("Science Calculator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(1080,760);
 		frame.setLayout(null);
 		
+		Menu.setBounds(0,0,1080, 45);
+		Menu.setFont(new Font("NewellsHand", Font.PLAIN, 30));
+		frame.add(Menu.getMenuBar());
 		
-		CalPanel.setBounds(10, 50, 700, 90);
+		CalPanel.setBounds(10, 50, 700, 110);
 		CalPanel.setFont(new Font("NewellsHand", Font.PLAIN, 30));
 		frame.add(CalPanel.getPanel());
 		
-		ResultArea.Panel.setBounds(10,170,700,100);
+		ResultArea.Panel.setBounds(10,170,700,110);
 		ResultArea.Panel.setFont(new Font("NewellsHand", Font.PLAIN, 30));
 		frame.add(ResultArea.Panel);
 		
-		HistoryArea.setBounds(720,50, 350, 650);
+		HistoryArea.setBounds(720,50, 350, 655);
 		HistoryArea.setFont(new Font("NewellsHand", Font.PLAIN, 30));
 		frame.add(HistoryArea.getPanel());
 		
@@ -41,12 +47,12 @@ public class ScienceCalculator extends Calculator implements ActionListener{
 		SymTable.Panel.setLayout(new GridLayout(7,5,1,1));
 
 		for (JButton button : SymTable.numButton.Buttons){
-            button.addActionListener(this);
-        }
+            		button.addActionListener(this);
+       		 }
         
-        for (JButton button : SymTable.operButton.Buttons){
-            button.addActionListener(this);
-        }
+        	for (JButton button : SymTable.operButton.Buttons){
+           		 button.addActionListener(this);
+        	}
 		frame.add(SymTable.Panel);
 
 		frame.setVisible(true);
