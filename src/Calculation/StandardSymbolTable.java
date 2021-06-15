5cdc84b351efbe4aeaae80628858cc02d292c65c
LeNgocYenNhi <82575213+LeNgocYenNhi@users.noreply.github.com>
2021-06-15 23:27:01 +0700

    Update StandardSymbolTable.java

--- src/Calculation/StandardSymbolTable.java
+++ src/Calculation/StandardSymbolTable.java
@@ -1,81 +1,41 @@
 package Calculation;
 
-import javax.swing.JPanel;
 import java.awt.*;
 
-public class StandardSymbolTable{
-	NumberButton numButton = new NumberButton();
-	OperatorButton operButton = new OperatorButton();
-	private JPanel Panel = new JPanel();
 
-	StandardSymbolTable(){
-		Panel.setBounds(10,400,1440,600);
-		Panel.setLayout(new GridLayout(5,5,1,1));
+public class StandardSymbolTable extends SymbolTable {
+		public StandardSymbolTable(){
+			creatSymbolTable();
+		}
+		@Override
+		public void creatSymbolTable() {
+			Panel.setBounds(10,400,1440,600);
+			Panel.setLayout(new GridLayout(5,5,1,1));
+
+			Panel.add(operButton.sqrt);
+			Panel.add(operButton.del);
+			Panel.add(operButton.clr);
+			Panel.add(operButton.div);
+
+			Panel.add(numButton.Buttons[7]);
+			Panel.add(numButton.Buttons[8]);
+			Panel.add(numButton.Buttons[9]);
+			Panel.add(operButton.mul);
+
+			Panel.add(numButton.Buttons[4]);
+			Panel.add(numButton.Buttons[5]);
+			Panel.add(numButton.Buttons[6]);
+			Panel.add(operButton.sub);
+
+			Panel.add(numButton.Buttons[1]);
+			Panel.add(numButton.Buttons[2]);
+			Panel.add(numButton.Buttons[3]);
+			Panel.add(operButton.add);
+
+			Panel.add(operButton.neg);
+			Panel.add(numButton.Buttons[0]);
+			Panel.add(operButton.dot);
+			Panel.add(operButton.equ);
+		}
 		
-		Panel.add(operButton.sqrt);
-		Panel.add(operButton.del);
-		Panel.add(operButton.clr);
-		Panel.add(operButton.div);
-
-		Panel.add(numButton.Buttons[7]);
-		Panel.add(numButton.Buttons[8]);
-		Panel.add(numButton.Buttons[9]);
-		Panel.add(operButton.mul);
-
-		Panel.add(numButton.Buttons[4]);
-		Panel.add(numButton.Buttons[5]);
-		Panel.add(numButton.Buttons[6]);
-		Panel.add(operButton.sub);
-		
-		Panel.add(numButton.Buttons[1]);
-		Panel.add(numButton.Buttons[2]);
-		Panel.add(numButton.Buttons[3]);
-		Panel.add(operButton.add);
-
-		Panel.add(operButton.neg);
-		Panel.add(numButton.Buttons[0]);
-		Panel.add(operButton.dot);
-		Panel.add(operButton.equ);
-
-	}
-	
-	public NumberButton getNumButton() {
-		return numButton;
-	}
-	public void setNumButton(NumberButton numButton) {
-		this.numButton = numButton;
-	}
-
-	public OperatorButton getOperButton() {
-		return operButton;
-	}
-	public void setOperButton(OperatorButton operButton) {
-		this.operButton = operButton;
-	}
-	
-	public void setLayout(GridLayout GridLayout){
-		this.Panel.setLayout(GridLayout);
-	}
-
-	public void setBackgroundColor(Color c){
-		this.Panel.setBackground(c);
-	}
-
-	public void setBounds(int x, int y, int width, int height){
-		this.Panel.setBounds(x, y, width, height);
-	}
-
-	public void setFont(Font f){
-		this.Panel.setFont(f);
-	}
-
-	public JPanel getPanel() {
-		return Panel;
-	}
-
-	public void setPanel(JPanel panel) {
-		Panel = panel;
-	}
-
-
 }
