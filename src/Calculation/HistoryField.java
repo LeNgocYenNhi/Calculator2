092850287de018b19e5ca78b5bab78e04f7efbcb
LeNgocYenNhi <82575213+LeNgocYenNhi@users.noreply.github.com>
2021-06-15 23:03:23 +0700

    Update HistoryField.java

--- src/Calculation/HistoryField.java
+++ src/Calculation/HistoryField.java
@@ -1,39 +1,17 @@
 package Calculation;
+
 import java.util.*;
 import java.util.List;
 
-import javax.swing.*;
-import java.awt.*;
 
-public class HistoryField {
+public class HistoryField extends Panel{ 
 
-	private JTextArea Panel = new JTextArea();
 	private List<String> list = new ArrayList<String>();  
-
-	public JTextArea getPanel() {
-		return Panel;
+	
+	public HistoryField (){
+		
 	}
-
-	public void setBounds(int x, int y, int width, int height){
-		this.Panel.setBounds(x, y, width, height);
-	}
-
-	public void setFont(Font f){
-		this.Panel.setFont(f);
-	}
-
-	public void setText(String text){
-		this.Panel.setText(text);
-	}
-
-	public String getText(){
-		return this.Panel.getText();
-	}
-
-	public void setPanel(JTextArea panel) {
-		Panel = panel;
-	}
-
+	
 	public List<String> getList() {
 		return list;
 	}
@@ -42,9 +20,6 @@ public class HistoryField {
 		this.list = list;
 	}
 	
-	HistoryField (){
-		Panel.setBounds(1460,10,450,980);
-	};
 
 	public void clear(){
 		this.list = new ArrayList<String>();  
