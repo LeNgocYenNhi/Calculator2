275d8dbf43bf7b7de5cea68b96e9efcc463d28b4
LeNgocYenNhi <82575213+LeNgocYenNhi@users.noreply.github.com>
2021-06-17 23:43:36 +0700

    Update ShowResultField.java

--- src/Calculation/ShowResultField.java
+++ src/Calculation/ShowResultField.java
@@ -4,7 +4,8 @@ public class ShowResultField extends Panel {
     double value;
 
     public ShowResultField(){
-       
+        getPanel().setBounds(10,170,700,110);
+    	getPanel().setFont(getFont());  
     }
 
     public void ShowResult(double value){
