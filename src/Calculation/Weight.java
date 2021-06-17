cc4174aa8056410f7c013d6b4d0eec98ea6a35d6
LeNgocYenNhi <82575213+LeNgocYenNhi@users.noreply.github.com>
2021-06-17 23:54:28 +0700

    Update Weight.java

--- src/Calculation/Weight.java
+++ src/Calculation/Weight.java
@@ -1,145 +1,125 @@
-  package Calculation;
-  import javax.swing.*;
-  import java.awt.*;
-  import java.awt.event.*;
-  import Calculation.Calculator;
-  public class Weight extends Calculator implements ActionListener{
+ package Calculation;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-    JFrame frame;
-    JPanel panel;
-    JButton enterButton;
-    JTextField toText;
-    JTextField fromText;
-    JLabel from, to;
-    JComboBox toBox, fromBox;
-    SymbolTable SymTable = getSymbolTable();
-    String sign;
-    double temp, result;
-    public Weight() {
-      frame = new JFrame("Weight");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setSize(600, 750);
-      frame.setLayout(null);
+public class Weight extends Convertor implements ActionListener{
 
-      from = new JLabel("From");
-        from.setBounds(20,50,75,25);
-        from.setFont(new Font("NewellsHand",Font.PLAIN, 30));
-        frame.add(from);
+	
+	protected ConvertSymbolTable SymTable;
+	protected ConvertingPanel CalPanel;
+	protected InputField Input;
+	protected OutputField Output;
+	protected ShowResultField ResultArea;
+	JComboBox toBox, fromBox;
+	String sign;
+	double temp, result;
+	public Weight() {
+		CalPanel = getConvertingPanel();
+		SymTable = getConvertSymbolTable();
+		ResultArea = getShowResultField();
+		Panel = getPanel();
+		Input = getInputField();
+		Output = getOutputField();
+		
+		frame = new JFrame("Weight");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(850, 600);
+		frame.setLayout(null);
+		
+	    Input.getPanel().setBounds(10, 110, 400, 50);
+		Input.getPanel().setFont(new Font("NewellsHand", Font.PLAIN, 30));
+		Input.setText("");
+		frame.add(Input.Panel);
+		
+		Output.getPanel().setBounds(10, 260, 400, 50);
+		Output.getPanel().setFont(new Font("NewellsHand",Font.PLAIN, 30));
+		Output.setText("");
+		frame.add(Output.Panel);
+	
+		String []  item = {"Kilograms", "Grams", "Pounds"};
+		fromBox = new JComboBox(item);
+		fromBox.setSelectedIndex(2);
+		fromBox.addActionListener(this);
+        fromBox.setBounds(10,50,150,50);
+        frame.add(fromBox);
+		
+       
+		toBox = new JComboBox(item);
+		toBox.addActionListener(this);
+        toBox.setBounds(10,200,150,50);
 
-        to = new JLabel("To");
-        to.setBounds(20,120,75,25);
-        to.setFont(new Font("NewellsHand",Font.PLAIN, 30));
-        frame.add(to);
-
-      fromText = new JTextField();
-      fromText.setBounds(150,50,180,40);
-      fromText.setFont(new Font("NewellsHand",Font.PLAIN, 30));
-      fromText.setText("");
-      frame.add(fromText);
-
-      enterButton = new JButton("Enter");
-      enterButton.setBounds(20,200,135,40);
-      enterButton.setFont(new Font("NewellsHand",Font.PLAIN, 30));
-      enterButton.setBackground(Color.WHITE);
-      enterButton.addActionListener(this);
-      frame.add(enterButton);
-
-      frame.add(enterButton);
-      SymTable.operButton.del.addActionListener(this);
-      frame.add(SymTable.operButton.del);
-      SymTable.operButton.dot.addActionListener(this);
-      frame.add(SymTable.operButton.dot);
-
-      String []  item = {"Kilograms", "Grams", "Pounds"};
-      fromBox = new JComboBox(item);
-      fromBox.setSelectedIndex(2);
-      fromBox.addActionListener(this);
-          fromBox.setBounds(400, 50, 150, 40);
-          frame.add(fromBox);
-
-
-      toText = new JTextField();
-      toText.setBounds(150,120,180,40);
-      toText.setFont(new Font("NewellsHand",Font.PLAIN, 30));
-      frame.add(toText);
-      toBox = new JComboBox(item);
-      toBox.addActionListener(this);
-          toBox.setBounds(400, 120, 150, 40);
-          frame.add(toBox);
-
-
-      panel = new JPanel();
-      panel.setBounds(5,300,572,400);
-      panel.setLayout(new GridLayout(4,3,1,1));
-
-      for (int i = 0; i < 10; i++) {
-        SymTable.numButton.Buttons[i].addActionListener(this);
-        panel.add(SymTable.numButton.Buttons[i]);
-      }
-
-      panel.add(SymTable.operButton.del);
-      panel.add(SymTable.operButton.dot);
-
-      frame.add(panel);
-      frame.setVisible(true);
-
-    }
-    public void actionPerformed(ActionEvent e) {
-
-      // TODO Auto-generated method stub
-      for(int i = 0; i < 10; i++) {
-        if(e.getSource() == SymTable.numButton.Buttons[i]) {//chỉ nhập được số (cho số thứ nhất)
-          fromText.setText(fromText.getText().concat(String.valueOf(i)));
-          temp = Double.parseDouble(fromText.getText());
+        frame.add(toBox);
+        
+		
+        SymTable.setBounds(435,10,390,540);
+		SymTable.setLayout(new GridLayout(4,3,1,1));
+		
+		ResultArea.getPanel().setBounds(10,315,417,233);
+		ResultArea.getPanel().setFont(new Font("NewellsHand", Font.PLAIN, 30));
+		frame.add(ResultArea.Panel);
+		
+		for (JButton button : SymTable.numButton.Buttons){
+            button.addActionListener(this);
         }
-      }
-      if(e.getSource() == SymTable.operButton.dot) {
-        fromText.setText(fromText.getText().concat("."));
+		SymTable.operButton.dot.addActionListener(this);
+		SymTable.operButton.del.addActionListener(this);
+		
+		frame.add(SymTable.getPanel());
+		frame.setVisible(true);
+		
+	}
+	public void actionPerformed(ActionEvent e) {
+		
+		
+		for(int i = 0; i < 10; i++) {
+			if(e.getSource() == SymTable.numButton.Buttons[i]) {
+				Input.setText(Input.getText().concat(String.valueOf(i)));
+				temp = Double.parseDouble(Input.getText());
+			}
+		}
+		if(e.getSource() == SymTable.operButton.dot) {
+			Input.setText(Input.getText().concat("."));
+			
+		}
+		if(e.getSource() == SymTable.operButton.del) {
+			String value = "";
+			Output.setText(String.valueOf(value));
+			Input.setText(String.valueOf(value));
+		}
 
-      }
-      if(e.getSource() == SymTable.operButton.del) {
-        String value = "";
-        toText.setText(String.valueOf(value));
-        fromText.setText(String.valueOf(value));
-      }
+			String valueFromBox = (String) fromBox.getSelectedItem();
+			String valueToBox = (String) toBox.getSelectedItem();
+			double medium = 0;
+			switch (valueFromBox){
+			case "Kilograms":
+				medium = temp;
+				break;
+			case "Grams":
+				medium = temp / 1000;
+				break;
+			case "Pounds":
+				medium = temp * 0.453592;
+			}
+			switch (valueToBox){
+			case "Kilograms":
+				result = medium;
+				break;
+			
+			case "Grams":
+				result = medium * 1000;
+				break;
+			case "Pounds":
+				result = medium * 2.204623;
+			}
+			Output.setText(String.valueOf(result));
+			}
+		
+		
+	
+	public static void main (String [] args) {
+		new Weight();
+	}
 
+}
 
-      if(e.getSource() == enterButton) {
-        String valueFromBox = (String) fromBox.getSelectedItem();
-        String valueToBox = (String) toBox.getSelectedItem();
-        double medium = 0;
-        switch (valueFromBox){
-        case "Kilograms":
-          medium = temp;
-          break;
-
-        case "Grams":
-          medium = temp / 1000;
-          break;
-        case "Pounds":
-          medium = temp * 0.453592;
-        }
-        switch (valueToBox){
-        case "Kilograms":
-          result = medium;
-          break;
-
-        case "Grams":
-          result = medium * 1000;
-          break;
-        case "Pounds":
-          result = medium * 2.204623;
-        }
-        toText.setText(String.valueOf(result));
-        }
-
-      }
-
-
-
-
-    public static void main (String [] args) {
-      Weight conver = new Weight();
-    }
-
-  }
